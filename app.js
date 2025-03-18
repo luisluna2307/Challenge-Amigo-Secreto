fe02b695694e9a2bb1d917ea8f14e38ef3ee0281
Luis Luna <laluna@espol.edu.ec>
2025-03-18 18:58:01 -0500

    final update

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-//variables
-//arreglos
-//funciones
-//eventos
-//objetos
-
 let inputAmigo = document.getElementById("amigo");
 let listaAmigos = [];
 let ulListaAmigos = document.getElementById("listaAmigos");
+let mostrarResultado = document.getElementById("resultado");
 function agregarAmigo() {
-    if (inputAmigo, value === "") {
+    if (inputAmigo.value === "") {
         alert("Debes ingresar un nombre");
     }
     else {
         listaAmigos.push(inputAmigo.value);
         ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
     }
+    limpiarCaja();
 
 }
 
 function sortearAmigo() {
+    let aleatorio = Math.floor(Math.random() * listaAmigos.length);
+    let amigoSorteado = listaAmigos[aleatorio];
+    mostrarResultado.innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
 
+
+}
+function limpiarCaja() {
+    document.querySelector("#amigo").value = "";
+    return;
 }
 
