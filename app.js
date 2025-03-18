b9e9e3a7a8fbb0b3548a2efc47a6fcb311bb063c
Luis Luna <laluna@espol.edu.ec>
2025-03-18 14:37:30 -0500

    Extraer informacion input y correcciones

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigo;
+//variables
+//arreglos
+//funciones
+//eventos
+//objetos
+
+let listaAmigos = [];
+let inputAmigo = document.getElementById("amigo");
